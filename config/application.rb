c309f2236e3d7102baff5b3b7056409b4316db0e
dikaemanchidi <dikaemanchidi@gmail.com>
2021-08-04 22:13:43 +0200

    make it better

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Everyleaf
         controller_specs: false,
         request_specs: false
     end
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
